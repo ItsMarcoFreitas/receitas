105b94d1e76c9cebd7315b09937717830d066828
Marco Freitas <marcoaureliobrr@gmail.com>
2025-01-31 11:25:30 -0300

    Update settings.py

--- receitas_site/settings.py
+++ receitas_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,9 +125,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Adicione ao final do arquivo
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Para produção
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")  # Onde seus arquivos CSS e JS estão na fase de desenvolvimento
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
